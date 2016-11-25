abb52b0d6f1768d97e6ac22f434b22c4f92cbc13
TKT2016 <bailanzi@aliyun.com>
2016-11-25 17:30:54 +0800

    提交遗漏文件

---
+++ ZLangRT/Attributes/ZCodeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLangRT.Attributes
+{
+    public class ZCodeAttribute : Attribute
+    {
+        public ZCodeAttribute(string procCode)
+        {
+            Code = procCode;
+        }
+
+        public string Code { get; private set; }
+    }
+}
